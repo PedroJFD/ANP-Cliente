0258d368282b940f645759ab56826cfcff98f03d
Pedro Joaquim <100968829+PedroJFD@users.noreply.github.com>
2024-09-01 17:09:01 -0400

    first commit

---
+++ ANP - Atividade - Cliente/Controllers/ClienteController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using ANP___Atividade___Cliente.Models;
+using ANP___Atividade___Cliente.Recursos;
+using ANP___Atividade___Cliente.Dtos;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ANP___Atividade___Cliente.Controllers
+{
+    [Route("Cliente")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        XmlCriador XmlCriador = new XmlCriador();
+        [HttpGet("{Id}")]
+        public IActionResult GetByed(int Id)
+        {
+            XmlCriador.CarregarXml();
+            var xml = XDocument.Load("DocumentoXml.XML");
+            var ClienteXml = xml.Descendants("Cliente").FirstOrDefault(x => (int)x.Element("Id") == Id);
+
+            if (ClienteXml == null)
+            {
+                return BadRequest("Cliente não encontrado.");
+            }
+
+            return Ok(ClienteXml.ToString());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] ClienteDTO item)
+        {
+            XmlCriador.CarregarXml();
+            var xml = XDocument.Load("DocumentoXml.XML");
+            int contador = xml.Descendants("Cliente").Count();
+
+            var cliente = new Cliente();
+
+            cliente.Id = contador + 1;
+            cliente.Nome = item.Nome;
+            cliente.DataNascimento = item.DataNascimento;
+            cliente.Sexo = item.Sexo;
+            cliente.Rg = item.Rg;
+            cliente.Cpf = item.Cpf;
+
+            if(ValidadorCPF.ValidaCPF(cliente.Cpf) == true)
+            {
+                cliente.Endereco = item.Endereco;
+                cliente.Cidade = item.Cidade;
+                cliente.Estado = item.Estado;
+                cliente.Telefone = item.Telefone;
+                cliente.Email = item.Email;
+
+                XmlCriador.GravarXml(cliente);
+
+                return StatusCode(StatusCodes.Status201Created, cliente);
+            }
+            else
+            {
+                return BadRequest("CPF Inválido.");
+            }
+        }
+
+        [HttpPut("{Id}")]
+        public IActionResult Put(int Id, [FromBody] ClienteDTO item)
+        {
+            XmlCriador.CarregarXml();
+            var xml = XDocument.Load("DocumentoXml.XML");
+            var ClienteXml = xml.Descendants("Cliente").FirstOrDefault(x => (int)x.Element("Id") == Id);
+
+            if (ClienteXml == null)
+            {
+                return BadRequest("Cliente não encontrado.");
+            }
+
+            ClienteXml.Element("Nome").Value = item.Nome;
+            ClienteXml.Element("DataNascimento").Value = item.DataNascimento.ToString("dd-MM-yyyy");
+            ClienteXml.Element("Sexo").Value = item.Sexo;
+            ClienteXml.Element("Rg").Value = item.Rg;
+            ClienteXml.Element("Cpf").Value = item.Cpf;
+
+            if (ValidadorCPF.ValidaCPF(ClienteXml.Element("Cpf").Value) == true)
+            {
+                ClienteXml.Element("Endereco").Value = item.Endereco;
+                ClienteXml.Element("Cidade").Value = item.Cidade;
+                ClienteXml.Element("Estado").Value = item.Estado;
+                ClienteXml.Element("Telefone").Value = item.Telefone;
+                ClienteXml.Element("Email").Value = item.Email;
+
+                return Ok(ClienteXml.ToString());
+            }
+            else
+            {
+                return BadRequest("CPF Inválido.");
+            }
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(int Id)
+        {
+            XmlCriador.CarregarXml();
+            var xml = XDocument.Load("DocumentoXml.XML");
+            var ClienteXml = xml.Descendants("Cliente").FirstOrDefault(x => (int)x.Element("Id") == Id);
+
+            if (ClienteXml == null)
+            {
+                return BadRequest("Cliente não encontrado.");
+            }
+
+            ClienteXml.Remove();
+            xml.Save("DocumentoXml.XML");
+
+            return Ok(ClienteXml.ToString());
+        }
+    }
+}
---
+++ ANP - Atividade - Cliente/Recursos/XmlCriador.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml;
+using System.IO;
+using ANP___Atividade___Cliente.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace ANP___Atividade___Cliente.Recursos
+{
+    internal class XmlCriador
+    {
+        private string xmlFilePath = "DocumentoXml.XML";
+        
+        public void CarregarXml()
+        {
+            XDocument DocumentoXml;
+
+            if (File.Exists(xmlFilePath))
+            {
+                DocumentoXml = XDocument.Load(xmlFilePath);
+            }
+            else
+            {
+                DocumentoXml = new XDocument(new XElement("Clientes"));
+                DocumentoXml.Save(xmlFilePath);
+            }
+        }
+        public void GravarXml(Cliente cliente)
+        {
+            XDocument DocumentoXml;
+            DocumentoXml = XDocument.Load(xmlFilePath);
+
+            XElement novoRegistro = new XElement("Cliente",
+                new XElement("Id", cliente.Id),
+                new XElement("Nome", cliente.Nome.Trim()),
+                new XElement("DataNascimento", cliente.DataNascimento),
+                new XElement("Sexo", cliente.Sexo.Trim()),
+                new XElement("Rg", cliente.Rg.Trim()),
+                new XElement("Cpf", cliente.Cpf.Trim()),
+                new XElement("Endereco", cliente.Endereco.Trim()),
+                new XElement("Cidade", cliente.Cidade.Trim()),
+                new XElement("Estado", cliente.Estado.Trim()),
+                new XElement("Telefone", cliente.Telefone.Trim()),
+                new XElement("Email", cliente.Email.Trim())
+            );
+
+            DocumentoXml.Root.Add(novoRegistro);
+            DocumentoXml.Save(xmlFilePath);
+        }
+    }
+}
